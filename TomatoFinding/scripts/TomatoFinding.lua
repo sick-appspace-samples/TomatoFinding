5d447739e9aef2c4d18e50d25412c36bb72b52cd
Florian Roser <Florian.Roser@sick.de>
2019-05-02 17:03:22 +0200

    Initial commit

---
+++ TomatoFinding/scripts/TomatoFinding.lua
@@ -0,0 +1,85 @@
+--[[----------------------------------------------------------------------------
+
+  Application Name:
+  TomatoFinding
+
+  Description:
+  Finding tomatoes by HSV conversion and segmentation of color image.
+
+  How to Run:
+  Starting this sample is possible either by running the app (F5) or
+  debugging (F7+F10). Setting breakpoint on the first row inside the 'main'
+  function allows debugging step-by-step after 'Engine.OnStarted' event.
+  Results can be seen in the image viewer on the DevicePage.
+  To run this sample a device with SICK Algorithm API is necessary.
+  For example InspectorP or SIM4000 with latest firmware. Alternatively the
+  Emulator on AppStudio 2.2 or higher can be used.
+
+  More Information:
+  Tutorial "Algorithms - Color".
+
+------------------------------------------------------------------------------]]
+--Start of Global Scope---------------------------------------------------------
+
+-- Delay in ms between visualization steps for demonstration purpose
+local DELAY = 1000
+
+-- Creating viewer
+local viewer = View.create()
+viewer:setID('viewer2D')
+
+-- Setting up graphical overlay attributes
+local textDecoration = View.TextDecoration.create()
+textDecoration:setPosition(20, 50)
+textDecoration:setSize(40)
+
+local decoration = View.ShapeDecoration.create()
+decoration:setFillColor(0, 255, 0, 100) -- Transparent green
+decoration:setLineColor(0, 230, 0) -- Green
+
+--End of Global Scope-----------------------------------------------------------
+
+--Start of Function and Event Scope---------------------------------------------
+
+-- Viewing image with text label
+--@show(img:Image, name:string)
+local function show(img, name)
+  viewer:addImage(img)
+  viewer:addText(name, textDecoration)
+  viewer:present()
+  Script.sleep(DELAY) -- for demonstration purpose only
+end
+
+local function main()
+  local img = Image.load('resources/Tomatoes.bmp')
+  viewer:clear()
+  viewer:addImage(img)
+  viewer:present()
+  Script.sleep(DELAY) -- for demonstration purpose only
+
+  -- Converting to HSV color space (Hue, Saturation, Value)
+  local H, S, V = img:toHSV()
+  show(H, 'Hue') -- View image with textDecoration label and delay
+  show(S, 'Saturation')
+  show(V, 'Value')
+
+  -- Threshold on saturation to find colored regions
+  local tomatoRegion = V:threshold(130, 255)
+  local tomatoes = tomatoRegion:findConnected(100)
+
+  -- Visualizing tomato borders/edges
+  viewer:add(img)
+  for i = 1, #tomatoes do
+    local border = tomatoes[i]:getBorderRegion()
+    border = border:dilate(5)
+    viewer:add(border, decoration)
+  end
+  viewer:present()
+  print(#tomatoes .. ' Tomatoes found')
+  print('App finished.')
+end
+--The following registration is part of the global scope which runs once after startup
+--Registration of the 'main' function to the 'Engine.OnStarted' event
+Script.register('Engine.OnStarted', main)
+
+--End of Function and Event Scope--------------------------------------------------
---
+++ readme.md
@@ -0,0 +1,15 @@
+## TomatoFinding
+Finding tomatoes by HSV conversion and segmentation of color image.
+### How to Run
+Starting this sample is possible either by running the App (F5) or
+debugging (F7+F10). Setting breakpoint on the first row inside the 'main'
+function allows debugging step-by-step after 'Engine.OnStarted' event.
+Results can be seen in the image viewer on the DevicePage.
+To run this sample a device with SICK Algorithm API is necessary.
+For example InspectorP or SIM4000 with latest firmware. Alternatively the
+Emulator on AppStudio 2.2 or higher can be used.
+### More Information
+Tutorial "Algorithms - Color"
+
+### Topics
+Algorithm, Image-2D, Color-Analysis, Sample, SICK-AppSpace
